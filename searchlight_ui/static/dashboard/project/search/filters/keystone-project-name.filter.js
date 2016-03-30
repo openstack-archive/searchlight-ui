07fb0372ed1577cfdd68464bee2683ceeb54923a
Travis Tripp <travis.tripp@hpe.com>
2016-03-29 18:58:40 -0600

    Add getProject policy check to project name filter
    The project column is enabled for admins by default, but can be enabled
    via searchlight_policy.json for all users.  When it is enabled, it will
    cause a lookup for the project name to be done based on the project id
    of the resource in the results. However, the keystone get_project
    api itself has a policy on it and any requests for a project name
    will fail if the current user doesn't have access rights to it.
    By default, non-admin users will get a forbidden result which causes
    horizon to log the current user out (horizon bug).
    
    This patch adds a policy check for 'identity', 'identity:get_project'
    before attempting to look up the project name.
    
    Implements-blueprint horizon-initial-search-panel-plugin
    Change-Id: I07123ea439fa0643fa73297a98fc70316414ab0e

--- searchlight_ui/static/dashboard/project/search/filters/keystone-project-name.filter.js
+++ searchlight_ui/static/dashboard/project/search/filters/keystone-project-name.filter.js
@@ -21,44 +21,80 @@
     .filter('keystoneProjectName', keystoneProjectNameFilter);
 
   keystoneProjectNameFilter.$inject = [
-    'horizon.app.core.openstack-service-api.keystone'
+    'horizon.app.core.openstack-service-api.keystone',
+    'horizon.app.core.openstack-service-api.policy'
   ];
 
   /**
    * @ngdoc filter
    * @name keystoneProjectNameFilter
    * @description
-   * Takes raw id from the API and returns the user friendly name if found.
+   * Takes a raw project ID and if policy allows, it will return the project name
+   * from the keystone API. Filters are evaluated many, many times and stateful
+   * filters are not able to be cached by angular. So this service performs heavy
+   * caching of the policy and project name queries in order to optimize performance.
    *
-   * @param {function} keystone keystone API
+   * @param {function} keystone The keystone API service.
+   *
+   * @param {function} policy The policy API service.
    *
    * @returns {String} User friendly name if found.
    */
-  function keystoneProjectNameFilter(keystone) {
+  function keystoneProjectNameFilter(keystone, policy) {
     var projectNameCache = {};
-    var lookupRequested = {};
+    var projectNameRequested = {};
+    var projectNameLookupAllowed = null;
 
-    function findProjectName(input) {
-      if (!input) {
-        return input;
+    function findProjectName(projectId) {
+      if (!projectId) {
+        return projectId;
       }
 
-      if (projectNameCache[input]) {
-        return projectNameCache[input];
+      if (projectNameCache[projectId]) {
+        return projectNameCache[projectId];
       }
 
-      projectNameCache[input] = input;
+      // Defaults to the projectId itself
+      projectNameCache[projectId] = projectId;
 
-      if (!lookupRequested[input]) {
-        keystone.getProject(input)
-          .then(cacheProjectName);
-        lookupRequested[input] = true;
-      }
-
-      function cacheProjectName(project) {
-        if (project) {
-          projectNameCache[input] = project.data.name;
+      // We only want to issue an API request once per project ID.
+      if (!projectNameRequested[projectId]) {
+        if (projectNameLookupAllowed) {
+          getProjectName(projectId);
+        } else if (projectNameLookupAllowed === null) {
+          getIfAllowed(projectId);
         }
+        projectNameRequested[projectId] = true;
+      }
+    }
+
+    function getIfAllowed(projectId) {
+      // The first page load with this filter may result in more than one policy API
+      // invocation. This is limited to the number of distinct project IDs that are requested
+      // the first time a page loads with this filter. This shouldn't be a real problem,
+      // because policy API provides basic client side caching. However, in filters it is best
+      // to avoid any excess processing and every policy API invocation will result in another
+      // promise, so we reduce the lookup cost to a simple boolean check after the first usage.
+      policy.ifAllowed({rules: [['identity', 'identity:get_project']]})
+        .then(
+          function onAllowed() {
+            projectNameLookupAllowed = true;
+            getProjectName(projectId);
+          },
+          function onNotAllowed() {
+            projectNameLookupAllowed = false;
+          }
+        );
+    }
+
+    function getProjectName(projectId) {
+      keystone.getProject(projectId)
+        .then(cacheProjectName);
+    }
+
+    function cacheProjectName(project) {
+      if (project) {
+        projectNameCache[project.data.id] = project.data.name;
       }
     }
 
